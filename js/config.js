4396bbd6963046606b709d40c8cf22fc27a520e4
MSNiranjan29 <adeptmohitsingh@outlook.com>
2024-12-09 12:19:12 +0530

    AnnaData

---
+++ js/config.js
@@ -0,0 +1,14 @@
+
+const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyBKUptdOXLW53holPk1AIfs345aXpHNqIA",
+  authDomain: "annadata-2024.firebaseapp.com",
+  databaseURL: "https://annadata-2024-default-rtdb.firebaseio.com",
+  projectId: "annadata-2024",
+  storageBucket: "annadata-2024.appspot.com",
+  messagingSenderId: "362080229861",
+  appId: "1:362080229861:web:ec20517ffc4ac6757db70c",
+  measurementId: "G-5KMJDN9BLC"
+});
+   const db = firebaseApp.firestore();
+   const auth = firebaseApp.auth();
+   
